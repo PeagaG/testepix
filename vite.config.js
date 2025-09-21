81a763c92c71cf091c9f2ecd3f0de84e8fda6932
PeagaG <paulohenrique.phrf15@gmail.com>
2025-09-21 18:20:17 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 // vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: 'public',              // onde está o index.html
+  root: "public", // ou "sitephp", se for essa a pasta do index.html
   build: {
-    outDir: '../dist',         // saída vai para /dist na raiz
-    emptyOutDir: true
+    outDir: "../dist",
+    emptyOutDir: true,
   },
   preview: {
-    port: parseInt(process.env.PORT || '4173'),
-    host: true
+    port: parseInt(process.env.PORT || "4173"),
+    host: true,
+    allowedHosts: ["meus-sites-sitephp.qkqaln.easypanel.host"]
   }
 });
